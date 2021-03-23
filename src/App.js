2e92ec3acced409492c09f114ef8d030c6baf16c
Zain-ul-Abedin87 <66695724+Zain-ul-Abedin87@users.noreply.github.com>
2021-03-23 11:29:08 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { uuid } from "uuidv4";
+import Header from "./Components/Header";
+import AddContact from "./Components/AddContact";
+import ContactList from "./Components/ContactList";
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+  const AddContactHandler = (contactItem) => {
+    console.log(contactItem);
+    setContacts([...contacts, { id: uuid(), ...contactItem }]);
+  };
+  const removeContactHandler = (id) => {
+    const newConatactList = contacts.filter((item) => {
+      return item.id != id;
+    });
+    setContacts(newConatactList);
+  };
+  useEffect(() => {
+    const reciveConatcts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (reciveConatcts) setContacts(reciveConatcts);
+  }, []);
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+  // const contact=[
+  //     {
+  //       id:"1",
+  //       name:"Zain Ul Abedin",
+  //       email:"Zain@gmail.com",
+  //     },
+  //     {
+  //       id:"1",
+  //       name:"Zain Ul Abedin",
+  //       email:"Zain@gmail.com",
+  //     }
+  //   ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <AddContact AddContactHandler={AddContactHandler} />
+      <ContactList contacts={contacts} getContactId={removeContactHandler} />
     </div>
   );
 }
 
 export default App;
+ 
